bbef12d4250c34242476d01250c85db469489e42
OmarSquircleArt <omar.squircleart@gmail.com>
2017-09-07 20:46:20 +0200

    Updated Transpose Array

--- animation_nodes/nodes/array/transpose_array.py
+++ animation_nodes/nodes/array/transpose_array.py
@@ -1,22 +1,17 @@
 import bpy
-from bpy.props import *
+
 from ... utils.layout import writeText
 from ... base_types import AnimationNode
 
 class TransposeArrayNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_TransposeArrayNode"
     bl_label = "Transpose Array"
-
-    errorMessage = StringProperty()
+    errorHandlingType = "EXCEPTION"
 
     def create(self):
         self.newInput("Array", "Array", "array")
         self.newInput("Integer List", "Axis", "axis")
         self.newOutput("Array", "SubArray", "newArray")
-
-    def draw(self, layout):
-        if self.errorMessage != "":
-            writeText(layout, self.errorMessage, icon = "ERROR", width = 50)
 
     def getExecutionCode(self, required):
         yield "try:"
@@ -26,5 +21,4 @@ class TransposeArrayNode(bpy.types.Node, AnimationNode):
             yield "    newArray = numpy.transpose(array, axis)"
         yield "    self.errorMessage = ''"
         yield "except Exception as e:"
-        yield "    subArray = array"
-        yield "    self.errorMessage = str(e)"
+        yield "    self.raiseErrorMessage(str(e))"

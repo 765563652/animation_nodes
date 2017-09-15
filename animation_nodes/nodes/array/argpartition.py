887921c3b4403a5b403b4d4c499a6e856e15d5e2
OmarSquircleArt <omar.squircleart@gmail.com>
2017-09-15 21:20:01 +0200

    Updated ArgPartition Node.

--- animation_nodes/nodes/array/argpartition.py
+++ animation_nodes/nodes/array/argpartition.py
@@ -14,5 +14,5 @@ class ArgPartitionNode(bpy.types.Node, AnimationNode):
         self.newInput("Integer", "Axis", "axis", value= -1)
         self.newOutput("Array", "Indices", "indices")
 
-    def getExecutionCode(self, required):
-        yield "indices = numpy.argpartition(array, kth, axis)"
+    def execute(self, array, kth, axis):
+        return numpy.argpartition(array, kth, axis)

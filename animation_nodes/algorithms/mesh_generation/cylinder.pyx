9aa3dcfafaffb77552f3bbbb046f1eafe72773d2
OmarSquircleArt <omar.squircleart@gmail.com>
2017-12-09 20:45:12 +0200

    Advanced Cylinder Mesh Initial Implementation.

--- animation_nodes/algorithms/mesh_generation/cylinder.pyx
+++ animation_nodes/algorithms/mesh_generation/cylinder.pyx
@@ -2,42 +2,123 @@ from libc.math cimport sin, cos
 from libc.math cimport M_PI as PI
 from ... data_structures cimport Vector3DList, EdgeIndicesList, PolygonIndicesList, Mesh
 
-def getCylinderMesh(float radius, float height, Py_ssize_t resolution, bint caps = True):
-    if resolution < 2:
-        raise Exception("resolution has to be >= 2")
-
-    return Mesh(
-        vertices(radius, height, resolution),
-        edges(resolution),
-        polygons(resolution, caps),
-        skipValidation = True)
-
 # Vertices
 ###########################################
 
-def vertices(float radius, float height, Py_ssize_t resolution):
-    if resolution < 2:
-        raise Exception("resolution has to be >= 2")
+def getCylinderMesh():
+    print("To Do.")
+
+def vertices(Py_ssize_t radialLoops, Py_ssize_t verticalLoops, Py_ssize_t innerLoops,
+             float outerRadius, float innerRadius, float height,
+             float startAngle, float endAngle,
+             bint mergeStartEnd, bint mergeCenter):
 
     cdef:
-        Py_ssize_t i
+        Py_ssize_t i, j, numVerts, dummyIndex
         Vector3DList vertices
-        float angleFactor = 2 * PI / resolution
-        float angle, x, y
+        float outerRadiusCos, outerRadiusSin, innerRadiusCos, innerRadiusSin, iRadius, newCos
+        float heightStep = height / (verticalLoops - 1)
+        float innerStep = (outerRadius if mergeCenter else outerRadius - innerRadius) / (innerLoops + 1)
+        float angleStep = (2 * PI if mergeStartEnd else endAngle - startAngle) / (radialLoops if mergeStartEnd else radialLoops - 1)
+        float iCos = cos(startAngle)
+        float iSin = sin(startAngle)
+        float eCos = cos(endAngle)
+        float eSin = sin(endAngle)
+        float stepCos = cos(angleStep)
+        float stepSin = sin(angleStep)
 
-    vertices = Vector3DList(length = resolution * 2)
-    for i in range(resolution):
-        angle = i * angleFactor
-        x = cos(angle) * radius
-        y = sin(angle) * radius
+    if mergeStartEnd:
+        if mergeCenter:
+            numVerts = radialLoops * (verticalLoops + innerLoops * 2) + 2
+        else:
+            numVerts = 2 * radialLoops * (verticalLoops + innerLoops)
+    else:
+        if mergeCenter:
+            numVerts = radialLoops * verticalLoops + 2 * (radialLoops +
+                                     verticalLoops - 2) * innerLoops + verticalLoops
+        else:
+            numVerts = 2 * (radialLoops * verticalLoops + innerLoops *
+                           (radialLoops + verticalLoops - 2))
 
-        vertices.data[i].x = x
-        vertices.data[i].y = y
-        vertices.data[i].z = 0
+    vertices = Vector3DList(length = numVerts, capacity = numVerts)
 
-        vertices.data[i + resolution].x = x
-        vertices.data[i + resolution].y = y
-        vertices.data[i + resolution].z = height
+    if not mergeStartEnd:
+        for i in range(verticalLoops):
+            for j in range(innerLoops):
+                iRadius = outerRadius - innerStep * (j + 1)
+                if mergeCenter:
+                    dummyIndex = radialLoops * (verticalLoops + innerLoops * 2) + i * innerLoops + j
+                else:
+                    dummyIndex = 2 * radialLoops * (verticalLoops + innerLoops) + i * innerLoops + j
+                vertices.data[dummyIndex].x = iCos * iRadius
+                vertices.data[dummyIndex].y = iSin * iRadius
+                vertices.data[dummyIndex].z = (i + 1) * heightStep
+
+    for i in range(radialLoops):
+        outerRadiusCos = iCos * outerRadius
+        outerRadiusSin = iSin * outerRadius
+        if not mergeCenter:
+            innerRadiusCos = iCos * innerRadius
+            innerRadiusSin = iSin * innerRadius
+
+        for j in range(verticalLoops):
+            dummyIndex = j * radialLoops + i
+            vertices.data[dummyIndex].x = outerRadiusCos
+            vertices.data[dummyIndex].y = outerRadiusSin
+            vertices.data[dummyIndex].z = j * heightStep
+
+            if not mergeCenter:
+                dummyIndex = radialLoops * (j + verticalLoops) + i
+                vertices.data[dummyIndex].x = innerRadiusCos
+                vertices.data[dummyIndex].y = innerRadiusSin
+                vertices.data[dummyIndex].z = j * heightStep
+
+        for j in range(innerLoops):
+            iRadius = outerRadius - innerStep * (j + 1)
+            if mergeCenter:
+                dummyIndex = radialLoops * verticalLoops + j * radialLoops + i
+            else:
+                dummyIndex = 2 * radialLoops * verticalLoops + j * radialLoops + i
+
+            vertices.data[dummyIndex].x = iCos * iRadius
+            vertices.data[dummyIndex].y = iSin * iRadius
+            vertices.data[dummyIndex].z = 0
+            dummyIndex += radialLoops * innerLoops
+            vertices.data[dummyIndex].x = iCos * iRadius
+            vertices.data[dummyIndex].y = iSin * iRadius
+            vertices.data[dummyIndex].z = height
+
+        newCos = stepCos * iCos - stepSin * iSin
+        iSin = stepSin * iCos + stepCos * iSin
+        iCos = newCos
+
+    if mergeStartEnd:
+        if mergeCenter:
+            dummyIndex = numVerts - 2
+            vertices.data[dummyIndex].x = 0
+            vertices.data[dummyIndex].y = 0
+            vertices.data[dummyIndex].z = 0
+            dummyIndex += 1
+            vertices.data[dummyIndex].x = 0
+            vertices.data[dummyIndex].y = 0
+            vertices.data[dummyIndex].z = height
+    else:
+        for i in range(verticalLoops):
+            for j in range(innerLoops):
+                iRadius = outerRadius - innerStep * (j + 1)
+                if mergeCenter:
+                    dummyIndex = radialLoops * (verticalLoops + innerLoops * 2) + innerLoops * (verticalLoops - 2) + i * innerLoops + j
+                else:
+                    dummyIndex = 2 * radialLoops * (verticalLoops + innerLoops) + innerLoops * (verticalLoops - 2) + i * innerLoops + j
+                vertices.data[dummyIndex].x = eCos * iRadius
+                vertices.data[dummyIndex].y = eSin * iRadius
+                vertices.data[dummyIndex].z = (i + 1) * heightStep
+        if mergeCenter:
+            for i in range(verticalLoops):
+                dummyIndex = radialLoops * (verticalLoops + innerLoops * 2) + innerLoops * (verticalLoops - 2) * 2 + i
+                vertices.data[dummyIndex].x = 0
+                vertices.data[dummyIndex].y = 0
+                vertices.data[dummyIndex].z = i * heightStep
 
     return vertices
 

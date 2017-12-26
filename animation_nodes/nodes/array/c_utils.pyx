a006c9e1840dd3acda8b9b1e761da7ab6871ae96
OmarSquircleArt <omar.squircleart@gmail.com>
2017-12-26 19:51:20 +0200

    Added arrayTo2DList function.

--- animation_nodes/nodes/array/c_utils.pyx
+++ animation_nodes/nodes/array/c_utils.pyx
@@ -2,12 +2,20 @@ import numpy
 cimport numpy
 import cython
 from libc.string cimport memcpy
-from ... data_structures cimport Vector3DList, DoubleList, EulerList
+from ... data_structures cimport Vector3DList, Vector2DList
 
 def arrayTo3DList(numpy.ndarray[float, ndim=2, mode="c"] input):
     cdef float* inputPointer = &input[0, 0]
     cdef int l = input.shape[0]
     cdef Py_ssize_t nbytes = input.nbytes
     cdef Vector3DList vectors = Vector3DList(length = l, capacity = l)
+    memcpy(vectors.data, inputPointer, nbytes)
+    return vectors
+
+def arrayTo2DList(numpy.ndarray[float, ndim=2, mode="c"] input):
+    cdef float* inputPointer = &input[0, 0]
+    cdef int l = input.shape[0]
+    cdef Py_ssize_t nbytes = input.nbytes
+    cdef Vector2DList vectors = Vector2DList(length = l, capacity = l)
     memcpy(vectors.data, inputPointer, nbytes)
     return vectors

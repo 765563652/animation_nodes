72e0aa4d9e6e2f2d4786fe5bbee81f1c194924b1
OmarSquircleArt <omar.squircleart@gmail.com>
2018-01-06 21:45:00 +0200

    Added Array To Float List Function.

--- animation_nodes/nodes/array/c_utils.pyx
+++ animation_nodes/nodes/array/c_utils.pyx
@@ -2,7 +2,15 @@ import numpy
 cimport numpy
 import cython
 from libc.string cimport memcpy
-from ... data_structures cimport Vector3DList, Vector2DList
+from ... data_structures cimport DoubleList, Vector3DList, Vector2DList
+
+def arrayToFloatList(numpy.ndarray[double, ndim=1, mode="c"] input):
+    cdef double* inputPointer = &input[0]
+    cdef int l = input.shape[0]
+    cdef Py_ssize_t nbytes = input.nbytes
+    cdef DoubleList values = DoubleList(length = l, capacity = l)
+    memcpy(values.data, inputPointer, nbytes)
+    return values
 
 def arrayTo3DList(numpy.ndarray[float, ndim=2, mode="c"] input):
     cdef float* inputPointer = &input[0, 0]
